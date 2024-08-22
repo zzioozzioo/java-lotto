1bd8d1d301a2ce4b2d4a4cc2a542ef85bbb945c2
zzioozzioo <zzioo1006@naver.com>
2024-08-22 20:44:44 +0900

    feat(Rank): add getter of the list 'rankList'

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -8,4 +8,11 @@ public class Rank {
     // 등수별(1~5등) 당첨 개수
     private List<Integer> rankList = new ArrayList<>(5);
 
+    public List<Integer> getRankList() {
+        return rankList;
+    }
+
+    public void setRankList(List<Integer> rankList) {
+        this.rankList = rankList;
+    }
 }
