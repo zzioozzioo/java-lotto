a3f236fb72b8e88e77de7102f32243826e52cfd7
zzioozzioo <zzioo1006@naver.com>
2024-08-22 23:38:34 +0900

    init(Input): initial commit

--- src/main/java/lotto/domain/Input.java
+++ src/main/java/lotto/domain/Input.java
@@ -1,6 +1,39 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
 
+    private String buyAmount;
 
+    private List<String> winningNumberList = new ArrayList<>();
+    private String bonusNumber;
+
+    public Input() {
+    }
+
+    public String getBuyAmount() {
+        return buyAmount;
+    }
+
+    public void setBuyAmount(String buyAmount) {
+        this.buyAmount = buyAmount;
+    }
+
+    public List<String> getWinningNumberList() {
+        return winningNumberList;
+    }
+
+    public void setWinningNumberList(List<String> winningNumberList) {
+        this.winningNumberList = winningNumberList;
+    }
+
+    public String getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(String bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
 }
