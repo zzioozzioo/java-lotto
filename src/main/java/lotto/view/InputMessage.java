bda870a56e901b568a6acc8378ac46b55f2e6ff3
zzioozzioo <zzioo1006@naver.com>
2024-08-23 00:36:29 +0900

    feat(InputMessage): add a function that gets a winning number

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -13,6 +13,10 @@ public class InputMessage {
     // TODO: 당첨 번호 입력 기능 구현
     public void getLottoNumber() {
 
+        // TODO: view에서 인덱싱하고 문자열 배열로 변환하고 이런 건 아닌 듯함
+        //  여기선 단순히 전달만 받고 나머지는 validator에서 해야 하나?
+        //  ㄴㄴ service에서 넘겨받아서 인덱싱하고 validator 호출하면 됨
+        input.setWinningNumber(readLine());
     }
 
     // TODO: 로또 구입 금액 입력 기능 구현
