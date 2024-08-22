4fea020e68a39d62182ab2552bd5281fc9486be0
zzioozzioo <zzioo1006@naver.com>
2024-08-22 20:07:47 +0900

    init(InputMessage): initial commit

---
+++ src/main/java/lotto/view/InputMessage.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+// View: 콘솔의 입출력 관리
+// 사용자 입력, 화면 출력
+public class InputMessage {
+
+    // TODO: 당첨 번호 입력 기능 구현
+    public void getLottoNumber() {
+    }
+
+    // TODO: 로또 구입 금액 입력 기능 구현
+    public void getUserInputAmount() {
+    }
+}
