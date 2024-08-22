0d577a27299f9492e06a224c36a37fa282ec6803
zzioozzioo <zzioo1006@naver.com>
2024-08-23 01:17:51 +0900

    feat(Validator): add a method 'isCorrectLength' to validate the winning number by checking the number of inputs and rename from 'isDigit' to 'isNumeric'

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -12,25 +12,34 @@ public class Validator {
      * 당첨 번호 입력 기능 예외 처리
      */
     // 6개의 당첨 번호에 대한 validation
-    public void validateDuplicateWinningNumber() {
-        isDigit();
+    public void validateWinningNumber() {
+        isNumeric();
+        isCorrectLength();
         isInRange();
-        isDuplicated();
-
-        // TODO: 사용자가 입력한 숫자가 6개인지 여부 체크하는 validation method 만들기
+        isWinningNumberDuplicated();
     }
 
-    public void isDigit() {
+    public void isNumeric() {
+
+
+    }
+
+    public void isCorrectLength() {
     }
 
     public void isInRange() {
     }
 
-    public void isDuplicated() {
+    public void isWinningNumberDuplicated() {
     }
 
     // 보너스 숫자에 대한 validation
-    public void validateDuplicateBonusNumber() {
+    public void validateBonusNumber() {
+        isNumeric();
+        isInRange();
+        isBonusNumberDuplicate();
+    }
+    public void isBonusNumberDuplicate() {
         // getWinningNumberList해서 해당 숫자가 있는지!! contains 쓰면 될 듯
         List<Integer> list = input.getWinningNumberList();
 
@@ -43,7 +52,7 @@ public class Validator {
      * 로또 구입 금액 입력 기능 예외 처리
      */
     public void validateInputAmount() {
-        isDigit();
+        isNumeric();
         isInRange();
         hasChange();
     }
