195d685527201f47a719da37e8cfdbda7d071495
zzioozzioo <zzioo1006@naver.com>
2024-08-22 20:05:43 +0900

    init: initial commit

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,28 @@ import java.util.List;
 public class Lotto {
 
     // TODO: final 제거 고민 - 당첨 번호가 불변이긴 한데,, 괜찮나?
-    private final List<Integer> winningNumberList = new ArrayList<>();
-    private final int bonusNumber = 0; // 새로 알게 된 점: final은 초기화가 필수인 듯
+    //  final로 지정하면 setter을 못 열어서 안 됨..
+
+    // TODO: setter 말고 constructor 사용하도록 변경해보기
+
+    private List<Integer> winningNumberList = new ArrayList<>();
+    private int bonusNumber; // 새로 알게 된 점: final은 초기화가 필수인 듯
+
+    public List<Integer> getWinningNumberList() {
+        return winningNumberList;
+    }
+
+    public void setWinningNumberList(List<Integer> winningNumberList) {
+        this.winningNumberList = winningNumberList;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+
 }
--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -8,6 +8,38 @@ public class User {
     private int winnings;
     private double rateOfReturn;
 
+    public int getBuyAmount() {
+        return buyAmount;
+    }
+
+    public void setBuyAmount(int buyAmount) {
+        this.buyAmount = buyAmount;
+    }
+
+    public int getLottoQuantity() {
+        return lottoQuantity;
+    }
+
+    public void setLottoQuantity(int lottoQuantity) {
+        this.lottoQuantity = lottoQuantity;
+    }
+
+    public int getWinnings() {
+        return winnings;
+    }
+
+    public void setWinnings(int winnings) {
+        this.winnings = winnings;
+    }
+
+    public double getRateOfReturn() {
+        return rateOfReturn;
+    }
+
+    public void setRateOfReturn(double rateOfReturn) {
+        this.rateOfReturn = rateOfReturn;
+    }
+
     // TODO: 사용자 로또 번호 리스트를 어떻게 구현..? 구매한 개수만큼 리스트를 만들어야 하나..
     //  배열 안에 리스트 ArrayList<ST>[] userNumberList = new ArrayList[로또 수량]; 이런 식으로?
 }
