0ca485bb00ebe8422d6a6788d7866746540b12e4
zzioozzioo <zzioo1006@naver.com>
2024-08-23 13:20:50 +0900

    feat(LottoService): add a function in method 'lottoNumberToList' that validates if winning number is numeric and add a method 'buyAmountToInt'

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -22,22 +22,52 @@ public class LottoService {
         String winningNumber = input.getWinningNumber();
         String[] winningNumberArr = winningNumber.split(",");
 
+        // TODO: winningNumberArr[i] 하나하나에 대해 isNumeric() 검증을 하는 방법
+        //  1. 얜 진짜 아닌 것 같음 ㅜㅜ
+        //  List<Integer> winningNumberList = IntStream.range(0, winningNumberArr.length)
+        //        .mapToObj(i -> {
+        //            String number = winningNumberArr[i];
+        //            if (isNumeric(number)) {
+        //                return Integer.parseInt(number);
+        //            } else {
+        //                throw new IllegalArgumentException("Invalid number format at index " + i + ": " + number);
+        //            }
+        //        })
+        //        .collect(Collectors.toList());
+        //  2. 그냥 for문 돌려서 인덱스 하나하나마다 validator.isNumeric() 호출
+        //  -> 얘는 매개변수 하나 생김, validateWinningNumber에서는 제외
+
+        for (String s : winningNumberArr) {
+            validator.isNumeric(s);
+        }
+
         List<Integer> winningNumberList = IntStream.range(0, winningNumberArr.length)
                 .mapToObj(i -> Integer.parseInt(winningNumberArr[i]))
                 .collect(Collectors.toList());
 
         input.setWinningNumberList(winningNumberList);
 
-        validator.validateDuplicateWinningNumber();
+        validator.validateWinningNumber();
     }
 
     public void bonusNumberToInt() {
         String bonusNumberString = input.getBonusNumberString();
-        int bonusNumber = Integer.parseInt(bonusNumberString);
+        validator.isNumeric(bonusNumberString);
 
+        int bonusNumber = Integer.parseInt(bonusNumberString);
         input.setBonusNumber(bonusNumber);
 
-        validator.validateDuplicateBonusNumber();
+        validator.validateBonusNumber();
+    }
+
+    public void buyAmountToInt() {
+        String buyAmountString = input.getBuyAmountString();
+        validator.isNumeric(buyAmountString);
+
+        int buyAmount = Integer.parseInt(buyAmountString);
+        input.setBuyAmount(buyAmount);
+
+        validator.validateInputAmount();
     }
 
 }
