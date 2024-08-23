dceaf6de67a116186740a7eb92be62a801504cf7
zzioozzioo <zzioo1006@naver.com>
2024-08-23 15:05:44 +0900

    refactor(Rank): change the type of 'Rank' from class to enum

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,18 +1,4 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Rank {
-
-    // 등수별(1~5등) 당첨 개수
-    private List<Integer> rankList = new ArrayList<>(5);
-
-    public List<Integer> getRankList() {
-        return rankList;
-    }
-
-    public void setRankList(List<Integer> rankList) {
-        this.rankList = rankList;
-    }
+public enum Rank {
 }
