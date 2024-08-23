b40b16969def62cfc1fa9bdac89a86347e10dbaf
zzioozzioo <zzioo1006@naver.com>
2024-08-23 13:25:49 +0900

    feat(Validator): complete all functions of class 'Validator' and added a common 'isnumeric' method

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -8,39 +8,61 @@ public class Validator {
 
     private Input input;
 
+    // TODO: input.getWinningNumberList이 중복되는데 클래스 전역 변수로 사용해도 되나..? 그건 좀 그렇겠지
+    // TODO: isNumeric()에서 문자열->문자 변환 다른 방법?
+
+    /**
+     * 공통 예외 처리
+     */
+    public void isNumeric(String s) {
+        if (!Character.isDigit(s.charAt(0))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     /**
      * 당첨 번호 입력 기능 예외 처리
      */
-    // 6개의 당첨 번호에 대한 validation
     public void validateWinningNumber() {
-        isNumeric();
         isCorrectLength();
         isInRange();
         isWinningNumberDuplicated();
     }
 
-    public void isNumeric() {
-
-
-    }
-
     public void isCorrectLength() {
+        if (input.getWinningNumberList().size() != 6) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void isInRange() {
+        if (!input.getWinningNumberList().stream().allMatch(digit -> digit >= 1 && digit <= 45)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void isWinningNumberDuplicated() {
+        if (input.getWinningNumberList().stream().distinct().count() != input.getWinningNumberList().size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    // 보너스 숫자에 대한 validation
+
+    /**
+     * 보너스 숫자 예외 처리
+     */
     public void validateBonusNumber() {
-        isNumeric();
-        isInRange();
+        isBonusNumberInRange();
         isBonusNumberDuplicate();
     }
+
+    private void isBonusNumberInRange() {
+        if (input.getBonusNumber() < 1 || input.getBonusNumber() > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public void isBonusNumberDuplicate() {
-        // getWinningNumberList해서 해당 숫자가 있는지!! contains 쓰면 될 듯
         List<Integer> list = input.getWinningNumberList();
 
         if (list.contains(input.getBonusNumber())) {
@@ -48,16 +70,25 @@ public class Validator {
         }
     }
 
+
     /**
      * 로또 구입 금액 입력 기능 예외 처리
      */
     public void validateInputAmount() {
-        isNumeric();
-        isInRange();
+        isInputAmountInRange();
         hasChange();
     }
 
+    private void isInputAmountInRange() {
+        if (input.getBonusNumber() < 1000 || input.getBonusNumber() > 100000) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public void hasChange() {
+        if (input.getBuyAmount() % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
 
