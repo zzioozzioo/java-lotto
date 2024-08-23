37b09d063ec297686f7bbd29d1c455771c2db217
zzioozzioo <zzioo1006@naver.com>
2024-08-23 15:46:24 +0900

    feat(Controller): add a method 'getBonusNumberInput'

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,10 +3,9 @@ package lotto.controller;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 
-import lotto.domain.Input;
 import lotto.domain.User;
 import lotto.domain.Winning;
-import lotto.service.LottoService;
+import lotto.service.ConvertService;
 import lotto.view.InputMessage;
 
 import java.util.List;
@@ -16,12 +15,12 @@ public class Controller {
     // 이렇게 의존성 주입을 하는 게 더 낫다고 함..!
     private User user;
     private Winning winning;
-    private LottoService lottoService;
+    private ConvertService lottoService;
     private InputMessage inputMessage;
 
-    public void getBuyAmount() {
+    public void getBuyAmountInput() {
         inputMessage.getUserBuyAmount();
-        int amount = lottoService.convertStringToInt(readLine());
+        int amount = lottoService.convertInputAmount(readLine());
         user.setBuyAmount(amount);
     }
 
@@ -29,6 +28,12 @@ public class Controller {
         inputMessage.getLottoWinningNumber();
         List<Integer> list = lottoService.convertStringToList(readLine());
         winning.setWinningNumberList(list);
+    }
+
+    public void getBonusNumberInput() {
+        inputMessage.getLottoBonusNumber();
+        int bonus = lottoService.convert(readLine());
+        winning.setBonusNumber(bonus);
     }
 
 }
