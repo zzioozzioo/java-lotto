47a1e43508b7b0d18e1a7d29bdaab46b200aac36
zzioozzioo <zzioo1006@naver.com>
2024-08-24 19:38:36 +0900

    refactor(DisplayResult): modify the method 'displayWinningRank'

--- src/main/java/lotto/view/DisplayResult.java
+++ src/main/java/lotto/view/DisplayResult.java
@@ -11,7 +11,6 @@ public class DisplayResult {
     private Rank rank;
     private User user;
 
-    // TODO: 사용자 로또 번호 출력 기능 구현
     public void displayBuyHowManyLotto() {
         System.out.println(user.getLottoQuantity() + "개를 구매했습니다.");
     }
@@ -29,11 +28,11 @@ public class DisplayResult {
 
         // TODO: 일치 개수별 당첨 개수는 user에 저장해야 하나 rank에 저장해야 하나
         // user.lottoResult 해시맵에 저장!!
-        System.out.println(rank.FIFTH.getCount() + "개 일치 (" + rank.FIFTH.getConvertPrize() + ") - " + "개");
-        System.out.println(rank.FOURTH.getCount() + "개 일치 (" + rank.FOURTH.getConvertPrize() + ") - " + "개");
-        System.out.println(rank.THIRD.getCount() + "개 일치 (" + rank.THIRD.getConvertPrize() + ") - " + "개");
-        System.out.println(rank.SECOND.getCount() + "개 일치, 보너스 볼 일치 (" + rank.SECOND.getConvertPrize() + ") - " + "개");
-        System.out.println(rank.FIRST.getCount() + "개 일치 (" + rank.FIRST.getConvertPrize() + ") - " + "개");
+        System.out.println(rank.FIFTH.getCount() + "개 일치 (" + rank.FIFTH.getConvertPrize() + ") - " + user.getLottoResult().get(rank.FIFTH) + "개");
+        System.out.println(rank.FOURTH.getCount() + "개 일치 (" + rank.FOURTH.getConvertPrize() + ") - " + user.getLottoResult().get(rank.FOURTH) + "개");
+        System.out.println(rank.THIRD.getCount() + "개 일치 (" + rank.THIRD.getConvertPrize() + ") - " + user.getLottoResult().get(rank.THIRD) + "개");
+        System.out.println(rank.SECOND.getCount() + "개 일치, 보너스 볼 일치 (" + rank.SECOND.getConvertPrize() + ") - " + user.getLottoResult().get(rank.SECOND) + "개");
+        System.out.println(rank.FIRST.getCount() + "개 일치 (" + rank.FIRST.getConvertPrize() + ") - " + user.getLottoResult().get(rank.FIRST) + "개");
     }
 
     public void displayRateOfReturn() {
