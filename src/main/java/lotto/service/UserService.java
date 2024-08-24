4e3df8bb31ac56abab46b1bf4736fd0b5482153e
zzioozzioo <zzioo1006@naver.com>
2024-08-24 14:05:22 +0900

    feat(UserService): add a method 'getRandomNumber' that picks 6 numbers that are not duplicated

--- src/main/java/lotto/service/UserService.java
+++ src/main/java/lotto/service/UserService.java
@@ -1,6 +1,20 @@
 package lotto.service;
 
+import lotto.domain.User;
+
+import java.util.Collections;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
 // 여기서 validation 호출?
 // TODO: 사용자 로또 번호 생성 기능 구현
 public class UserService {
+
+    private User user;
+
+    public void getRandomNumber() {
+        user.setUserNumberList(pickUniqueNumbersInRange(1, 45, 6));
+        // 중복 체크는 함수 내에서 다 하나? 그런 듯...
+    }
 }
