7337128fd119b8c74333142b5ff826f2cff01802
zzioozzioo <zzioo1006@naver.com>
2024-08-24 13:35:24 +0900

    feat(DisplayResult): add a function to print out 'userNumberList'

--- src/main/java/lotto/view/DisplayResult.java
+++ src/main/java/lotto/view/DisplayResult.java
@@ -14,6 +14,11 @@ public class DisplayResult {
         System.out.println(user.getLottoQuantity() + "개를 구매했습니다.");
     }
 
+    public void displayUserLottoNumber() {
+        // 여기서 수량만큼 반복을 하면 안 됨!! 이 메서드에서는 리스트에 저장된 값만 출력하면 되는 것
+        System.out.println(user.getUserNumberList()); // 사용자 로또 번호 출력
+    }
+
     public void displayWinningRank() {
 
         // TODO: 일치 개수별 당첨 개수는 user에 저장해야 하나 rank에 저장해야 하나
