25614c58b7363e6022521a2ecff06fd65e82f020
zzioozzioo <zzioo1006@naver.com>
2024-08-25 14:46:39 +0900

    refactor(ConvertService): extract methods as 'validateNumericArraty' and 'convertArrayToList'

--- src/main/java/lotto/service/ConvertService.java
+++ src/main/java/lotto/service/ConvertService.java
@@ -7,24 +7,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-// controller가 service 호출하는 것..
 public class ConvertService {
 
     private Validator validator = new Validator();
 
     public List<Integer> convertStringToList(String str) {
         String[] strArr = str.split(",");
-        for (String s : strArr) {
-            validator.isNumeric(s);
-        }
+        validateNumericArray(strArr);
 
-        List<Integer> list = IntStream.range(0, strArr.length)
-                .mapToObj(i -> Integer.parseInt(strArr[i]))
-                .collect(Collectors.toList());
+        List<Integer> list = convertArrayToList(strArr);
 
         validator.validateWinningNumber(list);
 
         return list;
+    }
+
+    public void validateNumericArray(String[] strArr) {
+        for (String s : strArr) {
+            validator.isNumeric(s);
+        }
+    }
+
+    public List<Integer> convertArrayToList(String[] strArr) {
+        return IntStream.range(0, strArr.length)
+                .mapToObj(i -> Integer.parseInt(strArr[i]))
+                .collect(Collectors.toList());
     }
 
     public int convertInputAmount(String str) {
@@ -42,6 +49,7 @@ public class ConvertService {
         validator.validateBonusNumber(num, winning);
         return num;
     }
+
     public int convert(String str) {
         int num = Integer.parseInt(str);
         return num;
