135f72e0bcd972c33a766ec0211624aaa99d77ba
zzioozzioo <zzioo1006@naver.com>
2024-08-25 14:47:39 +0900

    style(InputMessage): remove unnecessary code

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -1,12 +1,8 @@
 package lotto.view;
 
-import lotto.domain.Input;
-
 // View: 콘솔의 입출력 관리
 // 사용자 입력, 화면 출력
 public class InputMessage {
-
-    private Input input;
 
     public void getUserBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
