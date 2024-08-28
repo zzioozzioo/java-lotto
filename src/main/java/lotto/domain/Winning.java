e4b97a364b1cdf8526c73c7be2b898f5f67fc8c5
zzioozzioo <zzioo1006@naver.com>
2024-08-28 14:17:13 +0900

    refactor(Winning): modify member variables of 'Winning' class, add constructor and 'match' method and move the validation methods to this class

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -1,29 +1,70 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import lotto.constant.ConstNumber;
+import lotto.constant.ErrorMessage;
 
 public class Winning {
 
-    // TODO: setter 말고 constructor 사용하도록 변경해보기
+    private final Lotto lotto;
+    private final int bonusNumber;
 
-    private List<Integer> winningNumberList = new ArrayList<>();
-    private int bonusNumber;
-
-
-    public List<Integer> getWinningNumberList() {
-        return winningNumberList;
-    }
-
-    public void setWinningNumberList(List<Integer> winningNumberList) {
-        this.winningNumberList = winningNumberList;
-    }
-
-    public int getBonusNumber() {
-        return bonusNumber;
-    }
-
-    public void setBonusNumber(int bonusNumber) {
+    public Winning(Lotto lotto, int bonusNumber) {
+        validateBonusNumber(lotto, bonusNumber);
+        this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
+
+    public void validateBonusNumber(Lotto lotto, int bonusNumber) {
+        isBonusNumberInRange(bonusNumber);
+        isBonusNumberDuplicate(lotto, bonusNumber);
+    }
+
+    private void isBonusNumberInRange(int num) {
+        if (num < ConstNumber.LOTTO_FIRST_NUM.getNum() || num > ConstNumber.LOTTO_LAST_NUM.getNum()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_IN_RANGE_WINNING_NUM.getErrorMessage());
+        }
+    }
+
+    public void isBonusNumberDuplicate(Lotto lotto, int num) {
+        if (lotto.getNumbers().contains(num)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_WINNING_NUM.getErrorMessage());
+        }
+    }
+
+    public Rank match(Lotto lottoNumber) {
+        int count = lottoNumber.countMatch(lotto);
+        boolean bonus = lottoNumber.containNumber(bonusNumber);
+        return Rank.determineRank(count, bonus);
+    }
+
+    // 당첨 번호 입력받을 때 필요한 동작
+    // 1.
+
+    // 당첨 결과 생성에 필요한 동작
+    // 1. 당첨 번호와 사용자 구매 랜덤 번호 비교 -> service?
+    // 2. 일치 개수 계산 -> service
+    // 3. 일치 개수에 따른 rank 결정
+    // 4. 일치 개수별 결과 저장 -> controller
+
+    // 필요한 핵심 도메인 로직
+    // 1. 일치 개수에 따른 rank 결정
+    // return Rank,,,
+
+
+//    public List<Integer> getWinningNumberList() {
+//        return winningNumberList;
+//    }
+//
+//    public void setWinningNumberList(List<Integer> winningNumberList) {
+//        this.winningNumberList = winningNumberList;
+//    }
+//
+//    public int getBonusNumber() {
+//        return bonusNumber;
+//    }
+//
+//    public void setBonusNumber(int bonusNumber) {
+//        this.bonusNumber = bonusNumber;
+//    }
+
 }
