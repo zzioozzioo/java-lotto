8f8dc50b04da44432fa631625e63d01f8811df83
zzioozzioo <zzioo1006@naver.com>
2024-08-28 14:29:53 +0900

    refactor(DisplayMessage): modify parameters of method by using general variable and hashmap

--- src/main/java/lotto/view/DisplayMessage.java
+++ src/main/java/lotto/view/DisplayMessage.java
@@ -1,39 +1,39 @@
 package lotto.view;
 
 import lotto.domain.Rank;
-import lotto.domain.User;
 
+import java.util.HashMap;
 import java.util.List;
 
 
 public class DisplayMessage {
 
 
-    public void displayBuyHowManyLotto(User user) {
+    public void displayBuyHowManyLotto(int quantity) { // 완
         System.out.println();
-        System.out.println(user.getLottoQuantity() + "개를 구매했습니다.");
+        System.out.println(quantity + "개를 구매했습니다.");
     }
 
-    public void displayUserLottoNumber(List<Integer> numbers) {
+    public void displayUserLottoNumber(List<Integer> numbers) { // 완
         System.out.println(numbers);
     }
 
-    public void displayWinningStatistics() {
+    public void displayWinningStatistics() { // 완
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
     }
 
-    public void displayWinningRank(User user) {
+    public void displayWinningRank(HashMap<Rank, Integer> lottoResult) { // 완
 
-        System.out.println(Rank.FIFTH.getCount() + "개 일치 (" + Rank.FIFTH.getConvertPrize() + "원) - " + user.getLottoResult().get(Rank.FIFTH) + "개");
-        System.out.println(Rank.FOURTH.getCount() + "개 일치 (" + Rank.FOURTH.getConvertPrize() + "원) - " + user.getLottoResult().get(Rank.FOURTH) + "개");
-        System.out.println(Rank.THIRD.getCount() + "개 일치 (" + Rank.THIRD.getConvertPrize() + "원) - " + user.getLottoResult().get(Rank.THIRD) + "개");
-        System.out.println(Rank.SECOND.getCount() + "개 일치, 보너스 볼 일치 (" + Rank.SECOND.getConvertPrize() + "원) - " + user.getLottoResult().get(Rank.SECOND) + "개");
-        System.out.println(Rank.FIRST.getCount() + "개 일치 (" + Rank.FIRST.getConvertPrize() + "원) - " + user.getLottoResult().get(Rank.FIRST) + "개");
+        System.out.println(Rank.FIFTH.getCount() + "개 일치 (" + Rank.FIFTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIFTH) + "개");
+        System.out.println(Rank.FOURTH.getCount() + "개 일치 (" + Rank.FOURTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FOURTH) + "개");
+        System.out.println(Rank.THIRD.getCount() + "개 일치 (" + Rank.THIRD.getConvertPrize() + "원) - " + lottoResult.get(Rank.THIRD) + "개");
+        System.out.println(Rank.SECOND.getCount() + "개 일치, 보너스 볼 일치 (" + Rank.SECOND.getConvertPrize() + "원) - " + lottoResult.get(Rank.SECOND) + "개");
+        System.out.println(Rank.FIRST.getCount() + "개 일치 (" + Rank.FIRST.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIRST) + "개");
     }
 
-    public void displayRateOfReturn(User user) {
-        System.out.println("총 수익률은 " + user.getRateOfReturn() + "%입니다.");
+    public void displayRateOfReturn(double rateOfReturn) { // 완
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }
