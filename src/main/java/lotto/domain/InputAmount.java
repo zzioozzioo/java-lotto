4ab902ef5a4483a05dcdae8b2fff09e5f3bb9cba
zzioozzioo <zzioo1006@naver.com>
2024-08-28 19:13:30 +0900

    refactor(InputAmount): instead of removing getter method of 'buyAmount', add a method 'calculateLottoQuantity' that calculates the quantity of lotto and 'getTotalAmount' that returns the value of 'buyAmount'

--- src/main/java/lotto/domain/InputAmount.java
+++ src/main/java/lotto/domain/InputAmount.java
@@ -7,10 +7,6 @@ public class InputAmount {
 
     private final long buyAmount;
 
-    public long getBuyAmount() {
-        return buyAmount;
-    }
-
     public InputAmount(int buyAmount) {
         validate(buyAmount);
         this.buyAmount = buyAmount;
@@ -39,15 +35,11 @@ public class InputAmount {
         }
     }
 
-    // 사용자가 구매한 로또 번호 생성에 필요한 동작
-    // 1. 구입금액 입력받기 -> view에서 메시지 출력, service에서 입력받기
-    // 2. 입력된 금액이 유효한지 확인
-    // 3. 유효하다면 수량 계산 -> service에서, 그렇지 않다면 예외 내뿜기 -> 여기서
-    // 4. 수량만큼 반복(controller)해 번호 생성 -> Lotto에서
-    // 5. 번호 저장 -> controller에서(?)
+    public int calculateLottoQuantity() {
+        return (int)(buyAmount / ConstNumber.LOTTO_PRICE.getNum());
+    }
 
-    // 필요한 핵심 도메인 로직
-    // 1. 입력된 금액이 유효한지 확인
-    // -> 그러면 이름을 User 대신 InputAmount로 해야 하나? oo
-
+    public long getTotalAmount() {
+        return buyAmount;
+    }
 }
