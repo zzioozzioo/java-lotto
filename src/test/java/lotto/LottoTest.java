fc5fd53b4fd62187622489bd15d8b8240f43dbb4
zzioozzioo <zzioo1006@naver.com>
2024-08-30 14:54:09 +0900

    feat(LottoTest): add a test method to check methods 'countMatch' and 'containNumber' are functioning normally

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -30,5 +32,32 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 도메인 로직에 단위 테스트 추가로 작성하기
+    @DisplayName("일치하는 개수를 올바르게 계산해 반환한다.")
+    @Test
+    void getWrongMatchCount() {
+
+        //given
+        Lotto userLottoNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        int count = userLottoNumbers.countMatch(winningNumbers);
+
+        //then
+        assertEquals(count, 6);
+    }
+
+    @DisplayName("숫자를 포함하고 있는지 여부를 올바르게 판별해 반환한다.")
+    @Test
+    void containNumber() {
+        //given
+        Lotto userLottoNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int targetNumber = 1;
+
+        //when
+        boolean result = userLottoNumbers.containNumber(targetNumber);
+
+        //then
+        assertTrue(result);
+    }
 }
