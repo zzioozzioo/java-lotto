95ed9d88595bb05ac91bf4f2d4d200e1e4ec94c9
zzioozzioo <zzioo1006@naver.com>
2024-08-30 18:32:41 +0900

    style(Winning): remove unnecessary comments

--- src/main/java/lotto/view/DisplayMessage.java
+++ src/main/java/lotto/view/DisplayMessage.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class DisplayMessage {
 
 
-    public void displayBuyHowManyLotto(int quantity) { // 완
+    public void displayBuyHowManyLotto(int quantity) {
         System.out.println();
         System.out.println(quantity + "개를 구매했습니다.");
     }
@@ -18,13 +18,13 @@ public class DisplayMessage {
         System.out.println(numbers);
     }
 
-    public void displayWinningStatistics() { // 완
+    public void displayWinningStatistics() {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
     }
 
-    public void displayWinningRank(HashMap<Rank, Integer> lottoResult) { // 완
+    public void displayWinningRank(HashMap<Rank, Integer> lottoResult) {
 
         System.out.println(Rank.FIFTH.getCount() + "개 일치 (" + Rank.FIFTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIFTH) + "개");
         System.out.println(Rank.FOURTH.getCount() + "개 일치 (" + Rank.FOURTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FOURTH) + "개");
@@ -33,7 +33,7 @@ public class DisplayMessage {
         System.out.println(Rank.FIRST.getCount() + "개 일치 (" + Rank.FIRST.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIRST) + "개");
     }
 
-    public void displayRateOfReturn(double rateOfReturn) { // 완
+    public void displayRateOfReturn(String rateOfReturn) {
         System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }
