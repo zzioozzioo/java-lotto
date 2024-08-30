b63a9ada801de5cce71bdfe5444233c2843d2573
zzioozzioo <zzioo1006@naver.com>
2024-08-30 14:53:01 +0900

    feat(InputAmountTest): add test methods to check all methods in 'InputAmount' are functioning normally

---
+++ src/test/java/lotto/InputAmountTest.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import lotto.domain.InputAmount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InputAmountTest {
+
+    @DisplayName("범위에서 벗어난 금액을 입력하면 예외가 발생한다.")
+    @Test
+    void createInputAmountByWrongRange() {
+        assertThatThrownBy(() -> new InputAmount(500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 한 장의 금액으로 나누어 떨어지지 않는 금액을 입력하면 예외가 발생한다.")
+    @Test
+    void InputAmountHasChange() {
+        assertThatThrownBy(() -> new InputAmount(1500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 입력한 금액에 대한 로또의 수량이 올바르게 계산해 반환한다.")
+    @Test
+    void getLottoQuantity() {
+        //given
+        InputAmount amount = new InputAmount(8000);
+
+        //when
+        int quantity = amount.calculateLottoQuantity();
+
+        //then
+        assertEquals(quantity, 8);
+    }
+
+    @DisplayName("사용자가 입력한 금액을 올바르게 반환한다.")
+    @Test
+    void getInputAmount() {
+        //given
+        InputAmount amount = new InputAmount(8000);
+
+        //when
+        long totalAmount = amount.getTotalAmount();
+
+        //then
+        assertEquals(totalAmount, 8000);
+    }
+}
