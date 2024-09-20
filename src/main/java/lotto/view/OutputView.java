2c72aa7f653885c6f14025160ef3a57dc2dfbb97
zzioozzioo <zzioo1006@naver.com>
2024-09-20 17:28:00 +0900

    refactor(OutputView): rename the methods from 'display' to 'print'

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,25 +5,25 @@ import lotto.domain.Rank;
 import java.util.HashMap;
 import java.util.List;
 
-public class DisplayMessage {
+public class OutputView {
 
 
-    public void displayBuyHowManyLotto(int quantity) {
+    public void printBuyHowManyLotto(int quantity) {
         System.out.println();
         System.out.println(quantity + "개를 구매했습니다.");
     }
 
-    public void displayUserLottoNumber(List<Integer> numbers) { // 완
+    public void printUserLottoNumber(List<Integer> numbers) { // 완
         System.out.println(numbers);
     }
 
-    public void displayWinningStatistics() {
+    public void printWinningStatistics() {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
     }
 
-    public void displayWinningRank(HashMap<Rank, Integer> lottoResult) {
+    public void printWinningRank(HashMap<Rank, Integer> lottoResult) {
 
         System.out.println(Rank.FIFTH.getCount() + "개 일치 (" + Rank.FIFTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIFTH) + "개");
         System.out.println(Rank.FOURTH.getCount() + "개 일치 (" + Rank.FOURTH.getConvertPrize() + "원) - " + lottoResult.get(Rank.FOURTH) + "개");
@@ -32,7 +32,7 @@ public class DisplayMessage {
         System.out.println(Rank.FIRST.getCount() + "개 일치 (" + Rank.FIRST.getConvertPrize() + "원) - " + lottoResult.get(Rank.FIRST) + "개");
     }
 
-    public void displayRateOfReturn(String rateOfReturn) {
+    public void printRateOfReturn(String rateOfReturn) {
         System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }
